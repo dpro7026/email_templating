d5df867590515123b3d38ca627ae8c5e27f86d57
David Provest <dpro7026@uni.sydney.edu.au>
2017-11-08 13:37:56 +1100

    App generates an inlined CSS email from style.css and welcome.ejs files

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ http://127.0.0.1:3000/
 ## Inlining CSS
 Using the following package:
 https://www.npmjs.com/package/inline-css
+
+Look at these sites for email CSS inspiration:
+https://reallygoodemails.com/
+https://designschool.canva.com/blog/best-email-designs/ 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@ var router = express.Router();
 
 console.log('David\'s App Running..');
 
+// Inlining email CSS
+var inlineCss = require('inline-css');
+var fs = require('fs');
+var html = fs.readFileSync('./email/welcome.ejs');
+var css = fs.readFileSync('./email/style.css');
+var options = new Object();
+options['url'] = ' ';
+options['extraCss'] = css;
+
+inlineCss(html, options)
+    .then(function(html) { console.log(html); });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
